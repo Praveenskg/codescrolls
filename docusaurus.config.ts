8bfe8ab03ba9fcf3111356774a722381618f77c4
Praveen Singh <Praveencdg@gmail.com>
2026-02-03 20:26:49 +0530

    fix(config): update link handling to throw errors on broken links and duplicate routes fix(tsconfig): adjust compiler options and include paths for better type support

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,8 +13,8 @@ const config: Config = {
   organizationName: 'Praveenskg',
   projectName: 'codescrolls',
   trailingSlash: false,
-  onBrokenLinks: 'warn',
-
+  onBrokenLinks: 'throw',
+  onDuplicateRoutes: 'throw',
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -34,7 +34,7 @@ const config: Config = {
       headingIds: true,
     },
     hooks: {
-      onBrokenMarkdownLinks: 'warn',
+      onBrokenMarkdownLinks: 'throw',
     },
   },
 
